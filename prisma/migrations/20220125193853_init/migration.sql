0eec275cfce9b8bdb02b47ee960a6939c41bf743
Kristián Žuffa <kristian.zuffa@gmail.com>
2022-01-26 16:06:16 +0100

    prettier

--- prisma/migrations/20220125193853_init/migration.sql
+++ prisma/migrations/20220125193853_init/migration.sql
@@ -1,11 +1,10 @@
 -- CreateTable
 CREATE TABLE "User" (
-    "id" TEXT NOT NULL,
-    "email" TEXT NOT NULL,
-    "name" TEXT,
-    "password" TEXT,
-
-    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+  "id" TEXT NOT NULL,
+  "email" TEXT NOT NULL,
+  "name" TEXT,
+  "password" TEXT,
+  CONSTRAINT "User_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateIndex
